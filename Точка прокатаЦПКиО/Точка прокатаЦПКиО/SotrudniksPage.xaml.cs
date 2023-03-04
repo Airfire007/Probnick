ce96c8fe7b76cb0026ee8de912551bf7bd58dc31
Airfire007 <125852088+Airfire007@users.noreply.github.com>
2023-03-04 14:19:38 +0500

    Add files via upload

---
+++ Точка прокатаЦПКиО/Точка прокатаЦПКиО/SotrudniksPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Точка_прокатаЦПКиО
+{
+    /// <summary>
+    /// Логика взаимодействия для SotrudniksPage.xaml
+    /// </summary>
+    public partial class SotrudniksPage : Page
+    {
+        
+        public SotrudniksPage()
+        {
+            InitializeComponent();
+            DgridSotrudniks.ItemsSource = Точка_прокатаEntities.GetContext().Сотрудники.ToList();
+        }
+    }
+}
